f7904dc9d4bb72946df3ff484ef0bb5f5b2d0f22
Abdullah Haktan <abdullahhaktan9@gmail.com>
2025-08-22 22:03:08 +0300

    Add project files.

---
+++ FormıleSqlSorgularıOluşturma/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FormıleSqlSorgularıOluşturma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCalistir = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnEkleSilGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 312);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(1013, 174);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnCalistir
+            // 
+            this.btnCalistir.BackColor = System.Drawing.Color.Khaki;
+            this.btnCalistir.Location = new System.Drawing.Point(657, 12);
+            this.btnCalistir.Name = "btnCalistir";
+            this.btnCalistir.Size = new System.Drawing.Size(368, 175);
+            this.btnCalistir.TabIndex = 1;
+            this.btnCalistir.Text = "Çalıştır";
+            this.btnCalistir.UseVisualStyleBackColor = false;
+            this.btnCalistir.Click += new System.EventHandler(this.btnCalistir_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(639, 294);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnEkleSilGuncelle
+            // 
+            this.btnEkleSilGuncelle.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnEkleSilGuncelle.Location = new System.Drawing.Point(657, 193);
+            this.btnEkleSilGuncelle.Name = "btnEkleSilGuncelle";
+            this.btnEkleSilGuncelle.Size = new System.Drawing.Size(368, 113);
+            this.btnEkleSilGuncelle.TabIndex = 3;
+            this.btnEkleSilGuncelle.Text = "Ekle - Sil - Güncelle";
+            this.btnEkleSilGuncelle.UseVisualStyleBackColor = false;
+            this.btnEkleSilGuncelle.Click += new System.EventHandler(this.btnEkleSilGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 501);
+            this.Controls.Add(this.btnEkleSilGuncelle);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnCalistir);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCalistir;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnEkleSilGuncelle;
+    }
+}
+
---
+++ FormıleSqlSorgularıOluşturma/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FormıleSqlSorgularıOluşturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=ABDULLAH;Initial Catalog=DbOgrenciNot;Integrated Security=True;Encrypt=False;");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalistir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string sorgu;
+                sorgu = richTextBox1.Text;
+                SqlCommand cmd = new SqlCommand(sorgu, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sorgunuzu kontrol ediniz...", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEkleSilGuncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = richTextBox1.Text;
+            
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sorgu, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                conn.Open();
+                SqlCommand cmd1 = new SqlCommand("SELECT * FROM TBL_NOTLAR", conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd1);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Sorgunuzu kontrol ediniz...", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
